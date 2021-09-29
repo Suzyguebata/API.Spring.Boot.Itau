237e2f20a6ecc931a869b890309032344216e502
VanessaGomesdeOliveira <83438306+VanessaGomesdeOliveira@users.noreply.github.com>
2021-09-28 21:17:14 -0300

    Add files via upload

---
+++ springitautech/src/main/resources/springitautech/springitautech/src/test/java/com/itautech/springitautech/SpringitautechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itautech.springitautech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitautechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
