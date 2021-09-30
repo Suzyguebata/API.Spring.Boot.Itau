58b0f9234acefe3791ee67b61d5536bee5a02965
Suzyguebata <suzanaguebata@gmail.com>
2021-09-30 20:55:13 -0300

    anexo sql

---
+++ springitautech/src/main/sql/SQL dados.sql
@@ -0,0 +1,50 @@
+
+
+insert into tb_cliente values(
+	null, '334.474.568-01','lucas@itau.com','lucas santana','(31) 98758-8541'
+);
+insert into tb_cliente VALUES(
+	null, '541.456.987-98','caique@itau.com','Caique Lima','(11) 87944-4561'
+);
+
+insert into tb_cliente VALUES(
+	null, '142.456.789-14','baronny@itau.com','Baronny Castro','(86) 98741-1234'
+);
+
+insert into tb_cliente VALUES(
+	null, '456.987.789-18','gustavo@itau.com','Gustavo Benedito','(12) 96523-4785'
+);
+
+insert into tb_cliente VALUES(
+	null, '368.478.789-13','vanessa@itau.com','Vanessa Gomes','(11) 98709-4781'
+);
+
+insert into tb_conta VALUES(
+	null, '152',354125,15968.00,0,1
+);
+
+insert into tb_conta VALUES(
+	null, '541',369258,10009.00,0,3
+);
+
+insert into tb_conta VALUES(
+	null, '471',741852,109.00,0,4
+);
+
+insert into tb_conta VALUES(
+	null, '999',963254,12009.00,0,5
+);
+
+insert into tb_conta VALUES(
+	null, '777',741258,09.00,0,6
+);
+
+insert into tb_conta VALUES(
+	null, '152',354129,200000.00,2,1
+);
+
+insert into tb_conta VALUES(
+	null, '999',963299,5009.00,1,5
+);
+
+
